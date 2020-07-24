41e0b77317996830de51e88eb444e088ff3f00ea
Arun Rawat <rawatarun65@gmail.com>
2020-07-25 02:26:23 +0530

    Initial Commit

---
+++ 1_sum_of_two_number.py
@@ -0,0 +1,17 @@
+# Given two numbers num1 and num2. The task is to write a Python program to find the addition of these two numbers.
+
+# Examples:
+
+# Input: num1 = 5, num2 = 3
+# Output: 8
+
+# Input: num1 = 13, num2 = 6
+# Output: 19
+
+
+number1 = int(input("Enter the First Number : "))
+number2 = int(input("Enter the Second Number: "))
+
+sum = number1 + number2
+
+print("Sum of {0} and {1} is: {2}".format(number1, number2, sum))
---
+++ 2_factorial.py
@@ -0,0 +1,9 @@
+# Python Program for factorial of a number
+# Factorial of a non-negative integer, is multiplication of all integers smaller than or equal to n. For example factorial of 6 is 6*5*4*3*2*1 which is 720.
+
+def factorial(n):
+  return 1 if (n==1 or n==0) else n * factorial(n - 1);
+
+
+number = int(input("Enter the Number: "))
+print("Factorial of",number,"is", factorial(number)) 
